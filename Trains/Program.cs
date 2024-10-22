4c68d8edcf81246898b212f477909b680ef1b04a
Oskar Legner <oskarlegner@gmail.com>
2024-10-22 09:22:20 +0200

    init repo

---
+++ Trains/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Vehicle v = new Vehicle(17, "Yellow", new DateOnly(2001, 09, 11));
+
+Console.WriteLine(v.ToString());
+v.Run();
+
+class Vehicle
+{
+
+    private int numberOfWheels;
+    private string color;
+    private DateOnly productionDate;
+
+    public Vehicle(int numberOfWheels, string color, DateOnly productionDate)
+    {
+        if(numberOfWheels < 1)
+        {
+            throw new Exception("Vehicle should have at least 1 wheel");
+        } 
+
+        this.numberOfWheels = numberOfWheels;
+        this.color = color;
+        this.productionDate = productionDate;
+    }
+
+    public override string ToString()
+    {
+        return $"This is Vehicle.\n It has {numberOfWheels} wheels, {color} color and it was produced at {productionDate.ToString()}";
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("It is running");
+    }
+
+}
